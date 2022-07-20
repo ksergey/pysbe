e3032703b8715bfb83d9fa363145007786a12099
Sergey Kovalevich <inndie@gmail.com>
2022-07-20 19:03:55 +0300

    update

--- app/__main__.py
+++ app/__main__.py
@@ -1,11 +1,9 @@
 from app.schema import Schema
-from app.parser import Parser
 
 PATH = '/home/ksergey/dev/pysbe/resources/FixBinary.xml'
 
 def main() -> None:
-    parser = Parser(PATH)
-    schema = parser.schema
+    schema = Schema.loadFromFile(PATH)
     print(schema)
 
 if __name__ == '__main__':
--- app/schema.py
+++ app/schema.py
@@ -1,7 +1,25 @@
 from __future__ import annotations
-from dataclasses import dataclass, field
 from enum import Enum
-from typing import Optional, List, Union
+from typing import Optional
+import xml.etree.ElementTree as ElementTree
+
+def openXMLAndStripNs(path: str) -> ElementTree.Element:
+    file = open(path, 'r', encoding='utf-8')
+    it = ElementTree.iterparse(file)
+
+    # strip namespace
+    for _, el in it:
+        _, _, el.tag = el.tag.rpartition('}')
+
+    return it.root
+
+def injectRootAttr(node: ElementTree.Element, root: ElementTree.Element):
+    for child in node:
+        child.attrib['__root__'] = root
+        injectRootAttr(child, root)
+
+def getDocumentRoot(node: ElementTree.Element) -> ElementTree.Element:
+    return node.attrib.get('__root__')
 
 class Presence(Enum):
     REQUIRED = 'required'
@@ -12,124 +30,375 @@ class ByteOrder(Enum):
     LITTLE_ENDIAN = 'littleEndian'
     BIG_ENDIAN = 'bitEndian'
 
-@dataclass
+def cast(value: Optional[str], type) -> Optional[dest]:
+    if value:
+        return type(value)
+    return None
+
+class PrimitiveType:
+    def __init__(self, name: str, size: int, nullValue: str, minValue: str, maxValue: str):
+        self.name = name
+        self.size = size
+        self.nullValue = nullValue
+        self.minValue = minValue
+        self.maxValue = maxValue
+
+    @staticmethod
+    def get(name: str) -> PrimitiveType:
+        storage = {
+            'char': PrimitiveType('char', 1,  "CHAR_NULL", "CHAR_MIN", "CHAR_MAX"),
+            'int8': PrimitiveType('int8', 1,  "INT8_NULL", "INT8_MIN", "INT8_MAX"),
+            'int16': PrimitiveType('int16', 2,  "INT16_NULL", "INT16_MIN", "INT16_MAX"),
+            'int32': PrimitiveType('int32', 4,  "INT32_NULL", "INT32_MIN", "INT32_MAX"),
+            'int64': PrimitiveType('int64', 8,  "INT64_NULL", "INT64_MIN", "INT64_MAX"),
+            'uint8': PrimitiveType('uint8', 1,  "INT8_NULL", "INT8_MIN", "INT8_MAX"),
+            'uint16': PrimitiveType('uint16', 2,  "UINT16_NULL", "UINT16_MIN", "UINT16_MAX"),
+            'uint32': PrimitiveType('uint32', 4,  "UINT32_NULL", "UINT32_MIN", "UINT32_MAX"),
+            'uint64': PrimitiveType('uint64', 8,  "UINT64_NULL", "UINT64_MIN", "UINT64_MAX"),
+            'float': PrimitiveType('float', 4,  "FLOAT_NULL", "FLOAT_MIN", "FLOAT_MAX"),
+            'double': PrimitiveType('double', 8,  "DOUBLE_NULL", "DOUBLE_MIN", "DOUBLE_MAX")
+        }
+
+        return storage.get(name)
+
 class Type:
-    name: str
-    primitive_type: str
-    description: Optional[str] = None
-    presence: Presence = Presence.REQUIRED
-    null_value: Optional[str] = None
-    min_value: Optional[str] = None
-    max_value: Optional[str] = None
-    length: int = 1
-    offset: Optional[int] = None
-    semantic_type: Optional[str] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
-    value: Optional[str] = None
+    def __init__(self, root: ElementTree.Element) -> None:
+        self.name = root.attrib.get('name')
+        self.description = root.attrib.get('description', None)
+        self.presence = Presence(root.attrib.get('presence', 'required'))
+        self.nullValue = root.attrib.get('nullValue')
+        self.minValue = root.attrib.get('minValue')
+        self.maxValue = root.attrib.get('maxValue')
+        self.length = int(root.attrib.get('length', '1'))
+        self.offset = cast(root.attrib.get('offset', None), int)
+        self.primitiveType = PrimitiveType.get(root.attrib.get('primitiveType'))
+        self.semanticType = root.attrib.get('semanticType', None)
+        self.sinceVersion = int(root.attrib.get('sinceVersion', '0'))
+        self.deprecated = cast(root.attrib.get('deprecated', None), int)
+        self.valueRef = root.attrib.get('valueRef', None)
+        self._constValue = root.text.strip() if root.text else None
 
-@dataclass
+        if self.valueRef:
+            if self.valueRef.find('.') == -1:
+                raise Exception(f'valueRef ({self.valueRef}) format not valid (type: "{self.name}")')
+            if self.presence != Presence.CONSTANT:
+                raise Exception(f'presence must be "constant" when valueRef is set (type: "{self.name}")')
+
+        if self.presence == Presence.CONSTANT:
+            if self.valueRef is None:
+                if self._constValue is None:
+                    raise Exception(f'type presence is "constant" but XML node has no data (type: "{self.name}")')
+            else:
+                name, value = self.valueRef.split('.')
+                node = getDocumentRoot(root).find(f'.//types/enum[@name="{name}"]')
+                if node is None:
+                    raise Exception(f'type {name} not found (type: "{self.name}")')
+                if Enum(node).hasValue(value) is None:
+                    raise Exception(f'value {value} is not exists in enum {name} (type: "{self.name}")')
+
+        if self.nullValue is None:
+            self.nullValue = self.primitiveType.nullValue
+
+        if self.minValue is None:
+            self.minValue = self.primitiveType.minValue
+
+        if self.maxValue is None:
+            self.maxValue = self.primitiveType.maxValue
+
+    def __str__(self) -> str:
+        return f'Type(name="{self.name}", primitiveType={self.primitiveType.name}, encodedLength={self.encodedLength}, constValue={self._constValue})'
+
+    @property
+    def encodedLength(self) -> int:
+        if self.presence == Presence.CONSTANT:
+            return 0
+        return self.primitiveType.size * self.length
+
+    @property
+    def constValue(self) -> str:
+        if self.presence != Presence.CONSTANT:
+            raise Exception(f'type is not "constant" presence (type: "{self.name}")')
+        return self._constValue
+
 class Composite:
-    name: str
-    description: Optional[str] = None
-    offset: Optional[int] = None
-    semantic_type: Optional[str] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
-    elements: List[Union[Type, Composite, Ref, Enum, Set]] = field(default_factory=list)
+    def __init__(self, root: ElementTree.Element) -> None:
+        self.name = root.attrib.get('name')
+        self.offset = cast(root.attrib.get('offset', None), int)
+        self.description = root.attrib.get('description', None)
+        self.semanticType = root.attrib.get('semanticType', None)
+        self.sinceVersion = int(root.attrib.get('sinceVersion', '0'))
+        self.deprecated = cast(root.attrib.get('deprecated', None), int)
+        self.elements = Composite.loadElements(root)
 
-@dataclass
-class Ref:
-    name: str
-    type: str
-    offset: Optional[int] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
+        # check valid offset
+        offset = 0
+        for element in self.elements:
+            if element.offset:
+                if offset > element.offset:
+                    raise Exception(f'composite element has incorrect offset (composite: "{self.name}")')
+                offset = element.offset
+            else:
+                offset += element.encodedLength
 
-@dataclass
+    def __str__(self) -> str:
+        return f'Composite(name="{self.name}", elements=({len(self.elements)}), encodedLength={self.encodedLength})'
+
+    @staticmethod
+    def loadElements(root: ElementTree.Element) -> list:
+        result = []
+        for node in root:
+            if node.tag == 'type':
+                result.append(Type(node))
+            elif node.tag == 'composite':
+                result.append(Composite(node))
+            elif node.tag == 'enum':
+                result.append(Enum(node))
+            elif node.tag == 'set':
+                result.append(Set(node))
+        return result
+
+    @property
+    def encodedLength(self) -> int:
+        length = 0
+
+        for element in self.elements:
+            if element.offset:
+                length = element.offset
+            if element.presence != Presence.CONSTANT:
+                length += element.encodedLength
+
+        return length
+
 class Enum:
-    name: str
-    encoding_type: str
-    description: Optional[str] = None
-    offset: Optional[int] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
-    elements: List[ValidValue] = field(default_factory=list)
+    def __init__(self, root: ElementTree.Element) -> None:
+        self.name = root.attrib.get('name')
+        self.description = root.attrib.get('description', None)
+        self.sinceVersion = int(root.attrib.get('sinceVersion', '0'))
+        self.deprecated = cast(root.attrib.get('deprecated', None), int)
+        self.offset = cast(root.attrib.get('offset', None), int)
+        self.validValues = self.loadValidValues(root)
 
-@dataclass
-class ValidValue:
-    name: str
-    value: str
-    description: Optional[str] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
+        encodingTypeStr = root.attrib.get('encodingType')
+        if encodingTypeStr in [ 'char', 'int8', 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32', 'uint64' ]:
+            self.encodingType = PrimitiveType.get(encodingTypeStr)
+        else:
+            node = getDocumentRoot(root).find(f'.//types/type[@name="{encodingTypeStr}"]')
+            if node is None:
+                raise Exception(f'type {encodingTypeStr} not found (enum: "{self.name}")')
+            type = Type(node)
+            if type.length != 1:
+                raise Exception(f'illegal encodingType "{encodingTypeStr}" (enum: "{self.name}")')
+            self.encodingType = type.primitiveType
 
-@dataclass
+        self.nullValue =  root.attrib.get('nullValue', self.encodingType.nullValue)
+
+    def __str__(self) -> str:
+        return f'Enum(name="{self.name}", encodingType={self.encodingType.name}, encodedLength={self.encodedLength})'
+
+    @staticmethod
+    def loadValidValues(root: ElementTree.Element) -> list:
+        result = []
+        for node in root.findall('./validValue'):
+            result.append({
+                'name': node.attrib.get('name'),
+                'description': node.attrib.get('description', None),
+                'sinceVersion': int(node.attrib.get('sinceVersion', '0')),
+                'deprecated': cast(node.attrib.get('deprecated', None), int),
+                'value': node.text.strip()
+            })
+        return result
+
+    def hasValue(self, name: str) -> bool:
+        return len([entry for entry in self.validValues if entry['name'] == name]) > 0
+
+    @property
+    def encodedLength(self) -> int:
+        return self.encodingType.size
+
 class Set:
-    name: str
-    encoding_type: str
-    description: Optional[str] = None
-    offset: Optional[int] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
-    elements: List[Choice] = field(default_factory=list)
+    def __init__(self, root: ElementTree.Element) -> None:
+        self.name = root.attrib.get('name')
+        self.description = root.attrib.get('description', None)
+        self.sinceVersion = int(root.attrib.get('sinceVersion', '0'))
+        self.deprecated = cast(root.attrib.get('deprecated', None), int)
+        self.offset = cast(root.attrib.get('offset', None), int)
+        self.choices = self.loadChoices(root)
 
-@dataclass
-class Choice:
-    name: str
-    value: str
-    description: Optional[str] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
+        encodingTypeStr = root.attrib.get('encodingType')
+        if encodingTypeStr in [ 'uint8', 'uint16', 'uint32', 'uint64' ]:
+            self.encodingType = PrimitiveType.get(encodingTypeStr)
+        else:
+            node = getDocumentRoot(root).find(f'.//types/type[@name="{encodingTypeStr}"]')
+            if node is None:
+                raise Exception(f'type {encodingTypeStr} not found (enum: "{self.name}")')
+            type = Type(node)
+            if type.length != 1:
+                raise Exception(f'illegal encodingType "{encodingTypeStr}" (enum: "{self.name}")')
+            self.encodingType = type.primitiveType
 
-@dataclass
-class Field:
-    name: str
-    id: int
-    type: str
-    description: Optional[str] = None
-    offset: Optional[int] = 0
-    presence: Presence = Presence.REQUIRED
-    value_ref: Optional[str] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
+    def __str__(self) -> str:
+        return f'Set(name="{self.name}", encodingType={self.encodingType.name}, encodedLength={self.encodedLength})'
 
-@dataclass
-class Group:
-    name: str
-    id: int
-    description: Optional[str] = None
-    dimension_type: str = 'groupSizeEncoding'
-    elements: List[Union[Field, Group, Data]] = field(default_factory=list)
+    @property
+    def encodedLength(self) -> int:
+        return self.encodingType.size
 
-@dataclass
-class Data:
-    name: str
-    id: int
-    type: str
-    semantic_type: Optional[str] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
+    @staticmethod
+    def loadChoices(root: ElementTree.Element) -> list:
+        result = []
+        for node in root.findall('./choice'):
+            result.append({
+                'name': node.attrib.get('name'),
+                'description': node.attrib.get('description', None),
+                'sinceVersion': int(node.attrib.get('sinceVersion', '0')),
+                'deprecated': cast(node.attrib.get('deprecated', None), int),
+                'value': int(node.text.strip())
+            })
+        return result
 
-@dataclass
 class Message:
-    name: str
-    id: int
-    description: Optional[str] = None
-    block_length: Optional[int] = None
-    semantic_type: Optional[str] = None
-    since_version: int = 0
-    deprecated: Optional[int] = None
-    elements: List[Union[Field, Group, Data]] = field(default_factory=list)
+    def __init__(self, root: ElementTree.Element) -> None:
+        self.name = root.attrib.get('name')
+        self.id = int(root.attrib.get('id'))
+        self.description = root.attrib.get('description', None)
+        self.blockLength = cast(root.attrib.get('blockLength', None), int)
+        self.semanticType = root.attrib.get('semanticType', None)
+        self.sinceVersion = int(root.attrib.get('sinceVersion', '0'))
+        self.deprecated = cast(root.attrib.get('deprecated', None), int)
+        self.elements = self.loadElements(root)
 
-@dataclass
+    def __str__(self) -> str:
+        return f'Message(name="{self.name}", id="{self.id}", elements={len(self.elements)})'
+
+    @staticmethod
+    def loadElements(root: ElementTree.Element) -> list:
+        result = []
+        groupEncountered = False
+        for node in root:
+            if node.tag == 'field':
+                if groupEncountered:
+                    raise Exception('field node specified after group node')
+                result.append(Field(node))
+            elif node.tag == 'group':
+                result.append(Group(node))
+                groupEncountered = True
+
+        uniqueNames = { element.name for element in result }
+        if len(uniqueNames) != len(result):
+            raise Exception('duplicate elements name found (message: "{self.name}")')
+
+        uniqueIds = { element.id for element in result }
+        if len(uniqueIds) != len(result):
+            raise Exception('duplicate elements id found (message: "{self.name}")')
+
+        return result
+
+class Field:
+    def __init__(self, root: ElementTree.Element) -> None:
+        self.name = root.attrib.get('name')
+        self.id = int(root.attrib.get('id'))
+        self.description = root.attrib.get('description', None)
+        self.type = root.attrib.get('type')
+        self.offset = cast(root.attrib.get('offset', None), int)
+        self.presence = Presence(root.attrib.get('presence', 'required'))
+        self.valueRef = root.attrib.get('valueRef', None)
+        self.sinceVersion = int(root.attrib.get('sinceVersion', '0'))
+        self.deprecated = cast(root.attrib.get('deprecated', None), int)
+
+    def __str__(self) -> str:
+        return f'Field(name="{self.name}", id={self.id}, type={self.type})'
+
+class Group:
+    def __init__(self, root: ElementTree.Element) -> None:
+        self.name = root.attrib.get('name')
+        self.id = int(root.attrib.get('id'))
+        self.description = root.attrib.get('description', None)
+        self.dimensionType = root.attrib.get('dimensionType', 'groupSizeEncoding')
+        self.elements = self.loadElements(root)
+
+    def __str__(self) -> str:
+        return f'Group(name="{self.name}", id={self.id})'
+
+    @staticmethod
+    def loadElements(root: ElementTree.Element) -> list:
+        result = []
+        groupEncountered = False
+        for node in root:
+            if node.tag == 'field':
+                if groupEncountered:
+                    raise Exception('field node specified after group node')
+                result.append(Field(node))
+            elif node.tag == 'group':
+                result.append(Group(node))
+                groupEncountered = True
+
+        uniqueNames = { element.name for element in result }
+        if len(uniqueNames) != len(result):
+            raise Exception('duplicate elements name found (group: "{self.name}")')
+
+        uniqueIds = { element.id for element in result }
+        if len(uniqueIds) != len(result):
+            raise Exception('duplicate elements id found (group: "{self.name}")')
+
+        return result
+
 class Schema:
-    package: str
-    id: int
-    version: int
-    semantic_version: Optional[str] = None
-    byte_order: ByteOrder = ByteOrder.LITTLE_ENDIAN
-    description: Optional[str] = None
-    header_type: str = 'messageHeader'
-    types: List[Union[Type, Composite, Enum, Set]] = field(default_factory=list)
-    messages: List[Message] = field(default_factory=list)
+    def __init__(self, root: ElementTree.Element) -> None:
+        # inject reference to root for each element in doc
+        injectRootAttr(root, root)
+
+        self.types = self.loadTypes(root)
+        self.messages = self.loadMessages(root)
+
+    @staticmethod
+    def loadFromFile(path: str) -> Schema:
+        root = openXMLAndStripNs(path)
+        return Schema(root)
+
+    @staticmethod
+    def loadTypes(root: ElementTree.Element) -> dict:
+        result = {}
+
+        def insert(entry):
+            if entry.name in result:
+                raise Exception(f'type {entry.name} already exists')
+            result[entry.name] = entry
+
+
+        for node in root.findall('./types/type'):
+            entry = Type(node)
+            print(entry)
+            insert(entry)
+
+        for node in root.findall('./types/composite'):
+            entry = Composite(node)
+            print(entry)
+            insert(entry)
+
+        for node in root.findall('./types/enum'):
+            entry = Enum(node)
+            print(entry)
+            insert(entry)
+
+        for node in root.findall('./types/set'):
+            entry = Set(node)
+            print(entry)
+            insert(entry)
+
+        return result
+
+    @staticmethod
+    def loadMessages(root: ElementTree.Element) -> list:
+        result = {}
+
+        def insert(entry):
+            if entry.name in result:
+                raise Exception(f'message {entry.name} already exists')
+            result[entry.name] = entry
+
+        for node in root.findall('./message'):
+            entry = Message(node)
+            print(entry)
+            insert(entry)
+
+        return [*result.values()]
